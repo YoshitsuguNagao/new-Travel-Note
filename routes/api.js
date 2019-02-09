d38ac2860854d7f37e7df99b6ccb8b92c25aa35b
Madhur Suria Mata Garcia <madhurmata@Madhurs-MacBook-Pro.local>
2019-02-09 12:01:31 +0100

    get data from apiflight

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,16 @@ const axios = require('axios');
 const dateFormatChanger = require('../public/javascripts/dateFormatChanger')
 
 
-
 const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  axios.get('https://api.skypicker.com/flights?fly_from=BCN')
+  const { date_from, date_to, budget } = req.body;
+  axios.get(`https://api.skypicker.com/flights?fly_from=BCN&date_from=${date_from}`)
     .then(({ data }) => {
       console.log('vuelos', data);
       
-      res.render('trip');
+      // res.render('trip');
     })
     .catch((error) => {
       console.log('error', error);
@@ -21,17 +21,16 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const { date_from, date_to, budget } = req.body;
+  console.log(budget);
   console.log(dateFormatChanger(date_from));
-
-  console.log('is working')
-
-//   axios.get(`https://api.skypicker.com/flights?fly_from=BCN`)
-
-//   .then(() => {
-//     res.redirect('/profile');
-//   })
-//   .catch(next);
-// }
+  axios.get(`https://api.skypicker.com/flights?fly_from=BCN&date_from=${dateFormatChanger(date_from)}&curr=EUR&price_to=${budget}`)
+    .then((apiDataKiwi) => {
+      console.log(apiDataKiwi.data); 
+    })
+    .then(() => {
+      res.redirect('/trip');
+    })
+    .catch(next);
 });
 
 
