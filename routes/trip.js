db61a807d6aae4224bce9371f46f58fe803f7789
Yoshitsugu Nagao <yoshitsugunagao@gmail.com>
2019-02-12 19:58:23 +0100

    change the setting of flight API and modify trip class

--- routes/trip.js
+++ routes/trip.js
@@ -30,27 +30,35 @@ router.post('/', async (req, res, next) => {
   try {
     const { date_from, date_to, budget } = req.body;
     const tripDuration = getDuration(date_from, date_to);
-    const dateFrom = date_from.replace(/-/g, '');
-    const dateTo = date_to.replace(/-/g, '');
+    const departureDate = date_from.replace(/-/g, '');
+    const returnDate = date_to.replace(/-/g, '');
+    console.log(departureDate, ' <=> ', returnDate, ' duration', tripDuration);
     const cityList = await CityList.find();
-    const flightBudget = budget / 2;
-    const getFlightInfo = await axios.get(`https://api.skypicker.com/flights?fly_from=BCN&date_from=${dateFormatChanger(date_from)}&nights_in_dst_from=${tripDuration}&nights_in_dst_to=${tripDuration}&curr=EUR&price_to=${flightBudget}`);
+
+    /* Get flight info */
+    let flightBudget = budget / 2;
+    const getFlightInfo = await axios.get(`https://api.skypicker.com/flights?fly_from=BCN&date_from=${dateFormatChanger(date_from)}&date_to=${dateFormatChanger(date_from)}&return_from=${dateFormatChanger(date_to)}&return_to=${dateFormatChanger(date_to)}&curr=EUR&price_to=${flightBudget}&one_for_city=1`);
     const selectedFlightInfo = [];
+    const selectedAccommodationInfo = [];
     getFlightInfo.data.data.forEach((oneFlightData) => {
       const cityExist = cityList.some(city => city.cityName === oneFlightData.cityTo);
+      // console.log(oneFlightData.price,':',oneFlightData.cityTo)
       if (cityExist) {
         selectedFlightInfo.push(oneFlightData);
       }
     });
     const flightData = selectedFlightInfo[Math.floor(Math.random() * selectedFlightInfo.length)];
-    const accommodationBudget = budget - flightData.price;
+    console.log(flightData);
+    flightBudget = flightData.price;
 
+    /* Get Accommodation info */
+    const accommodationBudgetEur = budget - flightBudget;
     const cityName = flightData.cityTo;
     const cityInfo = await CityList.findOne({ cityName }, { cityId: 1, _id: 0 });
     console.log('cityId:', cityInfo, 'cityname:', cityName);
 
     // const start = new Date();
-    // const acc = await axios.all([getAccommodationList(cityInfo, dateFrom, dateTo), getAccommodationInfo(cityInfo)])
+    // const acc = await axios.all([getAccommodationList(cityInfo, departureDate, returnDate), getAccommodationInfo(cityInfo)])
     //   .then((axios.spread((accommodationList, accommodationInfo) => {
     //     console.log(accommodationList);
     //     console.log(accommodationInfo);
@@ -66,22 +74,30 @@ router.post('/', async (req, res, next) => {
     // const accommodationData2 = Object.getOwnPropertyDescriptor(acc[1].data.data, accommodationId);
     // console.log(accommodationData2);
 
-    const getAccommodationList = await axios.get(`http://developer.goibibo.com/api/cyclone/?app_id=${process.env.APP_IP}&app_key=${process.env.APP_KEY}&city_id=${cityInfo.cityId}&check_in=${dateFrom}&check_out=${dateTo}`);
+    const getAccommodationList = await axios.get(`http://developer.goibibo.com/api/cyclone/?app_id=${process.env.APP_IP}&app_key=${process.env.APP_KEY}&city_id=${cityInfo.cityId}&check_in=${departureDate}&check_out=${returnDate}`);
+    const accommodationBudgetInr = 79.72 * accommodationBudgetEur / tripDuration;
     const accommodationIdList = Object.keys(getAccommodationList.data.data);
-    const accommodationId = accommodationIdList[Math.floor(Math.random() * accommodationIdList.length)];
-    const accommodationData1 = Object.getOwnPropertyDescriptor(getAccommodationList.data.data, accommodationId);
+    for (let i = 0; i < accommodationIdList.length; i++) {
+      const accommodation = Object.getOwnPropertyDescriptor(getAccommodationList.data.data, accommodationIdList[i]);
+      if (accommodation.value.op < accommodationBudgetInr) {
+        selectedAccommodationInfo.push({ accommodationId: accommodationIdList[i], info: accommodation });
+      }
+    }
+    const accommodationData1 = selectedAccommodationInfo[Math.floor(Math.random() * selectedAccommodationInfo.length)];
     console.log(accommodationData1);
-
-    const getAccommodationInfo = await axios.get(`http://developer.goibibo.com/api/voyager/?app_id=${process.env.APP_IP}&app_key=${process.env.APP_KEY}&method=hotels.get_hotels_data&id_list=[${accommodationId}]&id_type=_id`);
-    const accommodationData2 = Object.getOwnPropertyDescriptor(getAccommodationInfo.data.data, accommodationId);
+    const getAccommodationInfo = await axios.get(`http://developer.goibibo.com/api/voyager/?app_id=${process.env.APP_IP}&app_key=${process.env.APP_KEY}&method=hotels.get_hotels_data&id_list=[${accommodationData1.accommodationId}]&id_type=_id`);
+    const accommodationData2 = Object.getOwnPropertyDescriptor(getAccommodationInfo.data.data, accommodationData1.accommodationId);
     console.log(accommodationData2);
 
+    console.log('flight: ', flightData.price, 'hotel: ', Math.ceil(accommodationData1.info.value.op / 79.72 * tripDuration));
+    const accommodationBudget = Math.ceil(accommodationData1.info.value.op / 79.72 * tripDuration);
     const data = { flightData, accommodationData1, accommodationData2 };
+    // console.log(data.flightData);
 
     res.render('trip', {
       data,
-      date_from,
-      date_to,
+      departureDate: date_from,
+      returnDate: date_to,
       budget,
     });
   } catch (error) {
@@ -89,8 +105,8 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-// function getAccommodationList(cityInfo, dateFrom, dateTo) {
-//   return axios.get(`http://developer.goibibo.com/api/cyclone/?app_id=${process.env.APP_IP}&app_key=${process.env.APP_KEY}&city_id=${cityInfo.cityId}&check_in=${dateFrom}&check_out=${dateTo}`);
+// function getAccommodationList(cityInfo, departureDate, returnDate) {
+//   return axios.get(`http://developer.goibibo.com/api/cyclone/?app_id=${process.env.APP_IP}&app_key=${process.env.APP_KEY}&city_id=${cityInfo.cityId}&check_in=${departureDate}&check_out=${returnDate}`);
 // }
 
 // function getAccommodationInfo(cityInfo) {
@@ -99,7 +115,11 @@ router.post('/', async (req, res, next) => {
 
 router.post('/save', (req, res, next) => {
   const { city } = req.body;
-  Trip.create({ city, owner: req.session.currentUser._id })
+  Trip.create({
+    city,
+    owner: req.session.currentUser._id,
+    flight: data.flight,
+  })
     .then(() => {
       res.redirect('/trip/my-trips');
     })
