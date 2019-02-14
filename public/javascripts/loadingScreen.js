82986bd0096cd6039b5891c9c5794b2c5de30d0d
Madhur Suria Mata Garcia <madhurmata@Madhurs-MacBook-Pro.local>
2019-02-14 15:31:23 +0100

    Working Loading buton

--- public/javascripts/loadingScreen.js
+++ public/javascripts/loadingScreen.js
@@ -2,7 +2,15 @@
 const flightBtn = document.querySelector('.trip-btn');
 
 function rotate() {
-  flightBtn.classList.add('rotate');
+  const departureImput = document.forms.tripForm.departureDate.value;
+  const returnImput = document.forms.tripForm.returnDate.value;
+  const budgetImput = document.forms.tripForm.budget.value;
+  if (departureImput === null || departureImput === '', returnImput === null || returnImput === '', budgetImput === null || budgetImput === '') {
+    return false;
+  } 
+    flightBtn.classList.add('rotate');
+  
+
   console.log('hola');
 }
 
