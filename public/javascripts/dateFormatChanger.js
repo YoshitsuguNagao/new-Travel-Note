0917fb937b16e0721da69deeaf1c19b75fb117cf
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-04-28 10:59:26 +0200

    bug fixed

--- public/javascripts/dateFormatChanger.js
+++ public/javascripts/dateFormatChanger.js
@@ -1,6 +1,7 @@
 function dateFormatChanger(date) {
-  const array = date.split("-")
-  return array[2] + "/" + array[1] + "/" + array[0];
+  const array = date.split('-');
+  return `${array[2]  }/${  array[1]  }/${  array[0]}`;
 }
+
 
 module.exports = dateFormatChanger;
